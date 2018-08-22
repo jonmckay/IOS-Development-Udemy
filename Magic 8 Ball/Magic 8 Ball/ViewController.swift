911ddbecc8d7c719796aaf2d11d2670e359e938c
Jon McKay <jmckay0411@gmail.com>
2018-08-21 19:00:32 -0600

    Add beginner IOS projects such as dicee

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Jonathan Mckay on 8/20/18.
+//  Copyright © 2018 Jonathan Mckay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"];
+    
+    var randomBallNumber : Int = 0;
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage();
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5));
+        ImageView.image = UIImage(named: ballArray[randomBallNumber]);
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        newBallImage();
+    }
+    
+}
+
---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 33
+
+let myName : String = "Jon"
+
+let myAgeInTenYears = myAge + 10
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.2
+
+let double : Double = 1.2329305
