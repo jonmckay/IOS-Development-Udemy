58df43164864b119ac9434cb5a2d56dd355b454b
Jon McKay <jmckay0411@gmail.com>
2018-08-22 21:00:18 -0600

    Show example of how a swift function returns a value

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,17 +1,34 @@
 
-func getMilk(howManyMilkCartons : Int){
+//func getMilk(howManyMilkCartons : Int){
+//    print("go to the shops")
+//    print("buy \(howManyMilkCartons) cartons of milk")
+//
+//    let priceToPay = howManyMilkCartons * 2
+//
+//    print("pay $\(priceToPay)")
+//    print("come home")
+//}
+
+func getMilk(howManyMilkCartons : Int, howMuchMoneyRobotWasGiven : Int) -> Int{
     print("go to the shops")
     print("buy \(howManyMilkCartons) cartons of milk")
-    
+
     let priceToPay = howManyMilkCartons * 2
-    
+
     print("pay $\(priceToPay)")
     print("come home")
+    
+    let change = howMuchMoneyRobotWasGiven - priceToPay
+    
+    return change
 }
+var amountOfChange = getMilk(howManyMilkCartons: 12, howMuchMoneyRobotWasGiven: 10);
 
+print("Hello master, here's your $\(amountOfChange) change")
 
-getMilk(howManyMilkCartons: 4);
-getMilk(howManyMilkCartons: 1);
-getMilk(howManyMilkCartons: 12);
+// Calling the getMilk() function
+//getMilk(howManyMilkCartons: 4);
+//getMilk(howManyMilkCartons: 1);
+//getMilk(howManyMilkCartons: 12);
 
 
