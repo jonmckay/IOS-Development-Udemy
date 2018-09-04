a5e7a2c862f6e1ad9909e45782bbffbac23dfaf5
Jon McKay <jmckay0411@gmail.com>
2018-09-03 19:09:03 -0600

    Fix xylophone to play different sounds on different key presses

--- Xylophone/Xylophone/ VIewController.swift
+++ Xylophone/Xylophone/ VIewController.swift
@@ -12,6 +12,8 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName : String = ""
+    
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
@@ -22,7 +24,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        let selectedSoundFileName : String = soundArray[sender.tag - 1]
+        selectedSoundFileName = soundArray[sender.tag - 1]
         
         print(selectedSoundFileName)
         
@@ -31,7 +33,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     func playSound() {
         
-        let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         
         do {
             try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
