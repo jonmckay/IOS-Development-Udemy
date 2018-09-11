993a8087d4cdac935758ebd497f06bc087847814
Jon McKay <jmckay0411@gmail.com>
2018-09-10 19:11:01 -0600

    Create an initial model class for the questions to be asked

---
+++ Quizzler/Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Jonathan Mckay on 9/10/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
