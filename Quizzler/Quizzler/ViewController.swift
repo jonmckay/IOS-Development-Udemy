20c6ac557151b2046886d01c7f06773a81d3f417
Jon McKay <jmckay0411@gmail.com>
2018-09-26 20:45:25 -0600

    Update UI with a running score and quiz question progress bar

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
     var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -42,11 +43,16 @@ class ViewController: UIViewController {
     
     func updateUI() {
         
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
     func nextQuestion() {
         if questionNumber <= 12 {
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
         }
         else {
             let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
@@ -64,6 +70,7 @@ class ViewController: UIViewController {
     func checkAnswer() {
         let correctAnswer = allQuestions.list[questionNumber].answer
         if pickedAnswer == correctAnswer {
+            score += 1
             print("You got it!")
         }
         else {
@@ -74,6 +81,7 @@ class ViewController: UIViewController {
     
     func startOver() {
         questionNumber = 0
+        score = 0
         nextQuestion()
     }
     
