7ff96c9dc74807fe4440066dd0e43a23c30007f4
Jon McKay <jmckay0411@gmail.com>
2018-10-10 22:17:12 -0600

    Experiment with stack views by building a calculator app

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Jonathan Mckay on 10/10/18.
+//  Copyright © 2018 Jonathan Mckay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
