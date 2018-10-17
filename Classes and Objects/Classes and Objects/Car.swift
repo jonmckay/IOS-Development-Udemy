fea2ccc684d641e55f6278fb7cb82e50a27ddc25
Jon McKay <jmckay0411@gmail.com>
2018-10-16 21:01:12 -0600

    Start advanced swift lessons by beginning console project

---
+++ Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,18 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Jonathan Mckay on 10/16/18.
+//  Copyright © 2018 Jonathan Mckay. All rights reserved.
+//
+
+import Foundation
+
+
+class Car {
+    
+    var color = "Black"
+    var numberOfSeats = 5
+    
+    
+}
---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Jonathan Mckay on 10/16/18.
+//  Copyright © 2018 Jonathan Mckay. All rights reserved.
+//
+
+import Foundation
+
+
+
