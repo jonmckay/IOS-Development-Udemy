5c1d1c17a39c9ad37b5336f784cb836882987b87
Jon McKay <jmckay0411@gmail.com>
2018-11-21 16:16:30 -0600

    Create weather object model to store retrieved weather data

--- Clima/Clima/WeatherViewController.swift
+++ Clima/Clima/WeatherViewController.swift
@@ -20,7 +20,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
     //TODO: Declare instance variables here
     let locationManager = CLLocationManager()
-
+    let weatherDataModel = WeatherDataModel()
     
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
@@ -77,7 +77,15 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Write the updateWeatherData method here:
     func updateWeatherData(json : JSON) {
         
-        let tempResults = json["main"]["temp"]
+        let tempResult = json["main"]["temp"].double
+        
+        weatherDataModel.temperature = Int(tempResult! - 273.15)
+        
+        weatherDataModel.city = json["name"].stringValue
+        
+        weatherDataModel.condition = json["weather"][0]["id"].intValue
+        
+        weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
     }
 
     
