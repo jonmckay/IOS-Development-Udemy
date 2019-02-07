64083b48883662340094fa9f0b9c010ca451bd49
Jon McKay <jmckay0411@gmail.com>
2019-02-06 17:09:19 -0700

    Practice implementing a function param to another function

---
+++ FunctionsInFunctions.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1, n2)
+}
+
+func add(n1: Int, n2: Int) -> Int {
+    return n1 + n2
+}
+
+func multiply(n1: Int, n2: Int) -> Int {
+    return n1 * n2
+}
+
+calculator(n1: 2, n2: 3, operation: add(n1:n2:))
