f561cedd2f3910ab59fd4739fd623e8e92a14f0d
Jon McKay <jmckay0411@gmail.com>
2019-02-06 17:24:34 -0700

    Convert multiply function into a closure and call it directly

---
+++ Closures.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1, n2)
+}
+
+//func multiply(n1: Int, n2: Int) -> Int {
+//    return n1 * n2
+//}
+
+calculator(n1: 2, n2: 3, operation: { (n1: Int, n2: Int) -> Int in
+    return n1 * n2
+})
