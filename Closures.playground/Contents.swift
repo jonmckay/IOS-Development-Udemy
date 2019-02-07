829dd2c8bfe0c4094a2c05c837690216b6990347
Jon McKay <jmckay0411@gmail.com>
2019-02-06 17:34:48 -0700

    Simplify closure even more by turning it into a trailing closure

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -8,4 +8,4 @@ func calculator(n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
 //    return n1 * n2
 //}
 
-calculator(n1: 2, n2: 3, operation: { $0 * $1 })
+calculator(n1: 2, n2: 3) { $0 * $1 }
